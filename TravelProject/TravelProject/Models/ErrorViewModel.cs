9bd47c6a25405c14e13532d7d18d338becbdbe4a
klas9802 <klas9806@gmail.com>
2024-02-17 20:55:58 +0800

    rm local

--- TravelProject/TravelProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TravelProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
